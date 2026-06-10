7f46bc603f7742a2adf7e7bc9bc758ffd74bcbad
Pranati Khichi <112482181+pranatisingh67485@users.noreply.github.com>
2026-06-10 15:43:10 +0530

    Add files via upload

---
+++ Emloyees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeManagmentProject
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public DateTime DOJ { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Service.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagmentProject
+{
+    public class Service
+    {
+        private Employee[] employees = new Employee[100];
+        private int employeeCount=-1;
+        private int nextID = 1;
+
+
+        public void AddEmployee()
+        {
+            if (employeeCount >= employees.Length)
+            {
+                Console.WriteLine("Employee storage is full!");
+                return;
+            }
+
+            Employee emp = new Employee();
+
+            emp.Id = nextID;
+
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+            if (!string.IsNullOrEmpty(name))
+            { 
+            emp.Name = name;
+            }
+
+            Console.Write("Enter Phone No: ");
+            emp.PhoneNo = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            emp.Email = Console.ReadLine();
+
+            Console.Write("Enter Address: ");
+            emp.Address = Console.ReadLine();
+            
+            employees[employeeCount] = emp;
+
+            Console.WriteLine($"{emp.Name} Added Successfully! ID: {emp.Id}");
+            employeeCount++;
+            nextID++;
+        }
+        public void ViewEmployees()
+        {
+            if (employeeCount == 0)
+            {
+                Console.WriteLine("No Employees Found!");
+                return;
+            }
+
+            Console.WriteLine("\n---------------------------------------------------------------------");
+            Console.WriteLine("ID\tName\t\tPhone\t\tEmail\t\t\tAddress");
+            Console.WriteLine("---------------------------------------------------------------------");
+
+            foreach (Employee employee in employees)
+            {
+                if (employee != null) 
+                {
+                    Console.Write(employee.Id + "\t" + employee.Name + "\t" + employee.Email + "\t" + "\t" + employee.PhoneNo + "\t" + employee.Address + "\n");
+                }
+                   
+                else
+                    break;
+
+            }
+        }
+
+        public void UpdateEmployee() {
+
+            Console.Write("Enter Employee ID: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < employeeCount; i++)
+            {
+                if (employees[i].Id == id)
+                {
+                    Console.Write("Enter New Name: ");
+                    employees[i].Name = Console.ReadLine();
+
+                    Console.Write("Enter New Phone No: ");
+                    employees[i].PhoneNo = Console.ReadLine();
+
+                    Console.Write("Enter New Email: ");
+                    employees[i].Email = Console.ReadLine();
+
+                    Console.Write("Enter New Address: ");
+                    employees[i].Address = Console.ReadLine();
+
+                    Console.WriteLine("Employee Updated Successfully!");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Employee Not Found!");
+        
+
+        }
+
+        public void GetEmployee() {
+
+        }
+
+        public void DeleteEmployee()
+        {
+
+        }
+
+       
+    }
+}
